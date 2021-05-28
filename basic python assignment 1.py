bf1051e8d3206f4f1804ff9500e38d469c7e8a0f
Chelsea Rodrigues <84131870+chelsea2311@users.noreply.github.com>
2021-05-28 12:13:10 +0530

    Add files via upload

---
+++ basic python assignment 1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. values-
+#     1.'hello'
+#     2. -87.8
+#     3. 6
+#     expression-
+#     1. *
+#     2. -
+#     3. /
+#     4. +
+# 
+# Q2. variable -1) Variable is a local or global instance where you can store data. It is a kind of means to store a value in a                    memory  location and change it if needed.
+#               2)eg. a=10
+#               3) You can store string inside a variable.
+#     String - 1) String is always enclosed inside "--" or '--'(double quote or single quote). They are sequence of characters.
+#              2) eg. s= "chelsea"
+#              3) You cannot store variable inside a string.
+# 
+# Q3. Different types of data types are:
+#     1) numeric
+#     2) set
+#     3) boolean
+#     4) dictionary
+# 
+# Q4. An expression is a combination of values and mathematical operators and they always evaluate them to a single value.
+#     eg. >>> 5*3
+#         >>> 15
+# 
+# Q5. Statement- 1)Statements represents actions or commands. 
+#                2)eg. assignment statements like spam=10,
+#                  print statements like 
+#                  >>> print("hello world") 
+#     Expression-1) An expression is a combination of values or operators and they always evaluate them to a single value.
+#                2) eg. >>> 7+5
+#                       >>> 12
+# 
+# Q6. bacon = 23
+# 
+# Q7. 'spamspamspam'
+# 
+# Q8. Variable name should always start with alphabets or underscore(_) . Variable name only includes alphabets and no special         charachter other than underscore(_) is allowed in variable name. That is why eggs is a valid variable name as it follows         the above conditions and 100 is invalid variable name as conditions are not satisfied.
+# 
+# Q9. the three functions are:
+#     1)int 
+#     2)flaot
+#     3)str
+#     
+# Q10. 'I have eaten' + 99 + ' burritos.' this expression cause an error because 99 is not written in single quote while other two       are written in single . we can fix this error by putting 99 in single quote('99')   
+
+# In[1]:
+
+
+bacon = 22
+bacon + 1
+
+
+# In[3]:
+
+
+'spam' + 'spamspam'
+'spam' * 3
+
+
+# In[4]:
+
+
+int(34)
+
+
+# In[5]:
+
+
+float(7.87)
+
+
+# In[10]:
+
+
+str("chelsea")
+
+
+# In[9]:
+
+
+'i have eaten' + '99' + 'burritos'
+
